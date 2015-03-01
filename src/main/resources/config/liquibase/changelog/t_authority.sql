98ffea6afbfea9433fe219b8d434f78f33aaa69d
bresliev <nikola.bresliev@gmail.com>
2015-03-01 22:38:42 +0100

    OK

---
+++ src/main/resources/config/liquibase/changelog/t_authority.sql
@@ -0,0 +1,6 @@
+/*
+-- Query: SELECT * FROM crm.t_authority
+-- Date: 2015-02-22 13:29
+*/
+INSERT INTO t_authority (name) VALUES ('ROLE_ADMIN');
+INSERT INTO t_authority (name) VALUES ('ROLE_USER');
